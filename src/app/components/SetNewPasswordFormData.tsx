f31e808196d63b329782d2daca502bf4aa4477b2
Beamlak <beamlaksolomon1357@gmail.com>
2025-08-09 22:11:57 +0300

    refractor: add validation for new password

--- src/app/components/SetNewPasswordFormData.tsx
+++ src/app/components/SetNewPasswordFormData.tsx
@@ -108,12 +108,12 @@ const SetNewPasswordForm = () => {
               }`}
               placeholder="********"
               {...register('newPassword', {
-                // required: 'New password is required',
-                // minLength: { value: 8, message: 'Password must be at least 8 characters' },
-                // pattern: {
-                //   value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                //   message: 'Password must include uppercase, lowercase, number, and special character',
-                // },
+                required: 'New password is required',
+                minLength: { value: 8, message: 'Password must be at least 8 characters' },
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                  message: 'Password must include uppercase, lowercase, number, and special character',
+                },
               })}
             />
             {errors.newPassword && (
